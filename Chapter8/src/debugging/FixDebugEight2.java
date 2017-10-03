a23404e66488b6e5e251525b348b27009b2f7a23
jthai2 <jimmythai35@gmail.com>
2017-10-03 09:26:47 -0500

    update

--- Chapter8/src/debugging/FixDebugEight2.java
+++ Chapter8/src/debugging/FixDebugEight2.java
@@ -8,7 +8,7 @@ public class FixDebugEight2
       int[] someNums = {4, 17, 22, 8, 35};
       int tot = 0;
       int x;
-      for(x = 0; x < someNums.length; --x)
+      for(x = 0; x < someNums.length; ++x)
          tot += someNums[x];
       System.out.println("Sum is " + tot);
       System.out.println("Average is " + tot * 1.0 / someNums.length);
